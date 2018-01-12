afcffffcb8a73af55df1e90cfe7a3964b855c719
peraltot <peraltot@gmail.com>
2018-01-12 13:54:44 -0500

    Updated schema to include steps table

--- schema.sql
+++ schema.sql
@@ -6,12 +6,6 @@ USE projects_db;
 CREATE TABLE user_table
 (
 	id int NOT NULL AUTO_INCREMENT,
-	person_name varchar(255) NOT NULL,
-    project_name varchar(255) NOT NULL,
-	project_type varchar(255) NOT NULL,
-	start_time DATE NOT NULL,
-	end_time DATE NOT NULL,
-    project_space varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
 
@@ -20,11 +14,23 @@ CREATE TABLE projects
 	id int NOT NULL AUTO_INCREMENT,
 	project_name varchar(255) NOT NULL,
 	project_type varchar(255) NOT NULL,
+	project_description varchar(512) NOT NULL,
 	start_time DATE NOT NULL,
 	end_time DATE NOT NULL,
     project_space varchar(255) NOT NULL,
     user_table_id int NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (user_table_id) REFERENCES user_table(id)
+);
+
+CREATE TABLE steps
+(
+	id int NOT NULL AUTO_INCREMENT,
+	steps_name varchar(255) NOT NULL,
+	step_description varchar(512) NOT NULL,
+	completed BOOLEAN default false,
+    projects_id int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (projects_id) REFERENCES projects(id)
 );
 
