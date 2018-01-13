5b381ca112e1cc08fbd7a1b74c2720bd8e1fc053
chaseleinart <chaseleinart@gmail.com>
2018-01-13 13:22:13 -0500

    removed extraneous code commited in a hurry :|

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,17 +1,10 @@
-module.exports = function(app) {
-
-    app.get("/", function(req, res) {
-        res.render("index");
-    })
-}
-
-
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
 
 // Dependencies
 // =============================================================
+
 var path = require("path");
 
 // Routes
@@ -20,7 +13,7 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // root route loads login.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
