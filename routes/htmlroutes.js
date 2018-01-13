16b0a4ff7f28569470830debb9f0d82a4913539a
chaseleinart <chaseleinart@gmail.com>
2018-01-13 13:17:45 -0500

    pull in new additions for commit

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,3 +4,26 @@ module.exports = function(app) {
         res.render("index");
     })
 }
+
+
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+
+};
