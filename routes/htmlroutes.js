f82f80ded79347601affaacd4dbc49d1acb4df53
emmapankey <emmapankey19@gmail.com>
2018-01-18 21:41:51 -0500

    added index route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -9,16 +9,22 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // root route loads login.html
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-  
-  app.get("/about", function(req, res) {
+
+  // load the index page
+  app.get("/index", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+  });
+
+  // loads the about page
+  app.get("/about", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/about.html"));
   });
 }
