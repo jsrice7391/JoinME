e723668003c87157338872d9a99243618df19685
chaseleinart <chaseleinart@gmail.com>
2018-01-19 18:19:01 -0500

    project api routes for review

--- routes/projects_api_routes.js
+++ routes/projects_api_routes.js
@@ -3,19 +3,7 @@ var path = require("path");
 
 module.exports = function(app) {
     
-      // GET route for getting all of the projects
-    //   app.get("/api/projects", function(req, res) {
-    //     var query = {};
-    //     if (req.query.user_id) {
-    //       query.UserId = req.query.user_id;
-    //     }
-    //     db.Project.findAll({
-    //       where: query
-    //     }).then(function(dbProject) {
-    //       res.json(dbProject);
-    //     });
-    //   });
-    
+//get all projects
 
       app.get("/api/projects", function(req, res) {
         db.Project.findAll({}).then(function(results) {
@@ -23,48 +11,22 @@ module.exports = function(app) {
         })
     });
      
-    
-      // POST route for saving a new project
-      app.post("/api/projects", function(req, res) {
-        db.Project.create(req.body).then(function(dbProject) {
-          res.json(dbProject);
-        });
-      });
+
+    //create a project
+    //note that description is misspelled in model
 
       app.post("/api/projects", function(req, res) {
         console.log(req.body)
         db.Project.create({
             Project_name: req.body.Project_name,
             Project_type: req.body.Project_type,
-            Project_description: req.body.Project_description
-    
+            Project_descripton: req.body.Project_descripton,
+            UserId: req.body.UserId
         }).then(function(results) {
             //   
-            console.log("That worked");
+            console.log("This project was created.");
         });
     });
     
-      // DELETE route for deleting projects
-      app.delete("/api/projects/:id", function(req, res) {
-        db.Project.destroy({
-          where: {
-            id: req.params.id
-          }
-        }).then(function(dbProject) {
-          res.json(dbProject);
-        });
-      });
-    
-      // PUT route for updating projects
-      app.put("/api/projects", function(req, res) {
-        db.Project.update(
-          req.body,
-          {
-            where: {
-              id: req.body.id
-            }
-          }).then(function(dbProject) {
-            res.json(dbProject);
-          });
-      });
+
     };
