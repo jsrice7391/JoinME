2b1d44a0791eadca853ae5b68c73b7a5a01ca02a
emmapankey <emmapankey19@gmail.com>
2018-01-22 20:08:50 -0500

    fixed the start app in server.js

--- server.js
+++ server.js
@@ -42,8 +42,10 @@ require("./routes/htmlroutes")(app, passport);
 require('./routes/authRoutes.js')(app, passport);
 
 // Start the app
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-        console.log("APP is listening on Port: " + PORT);
+app.listen(PORT, function() {
+    console.log("APP is listening on Port: " + PORT);
+    
+db.sequelize.sync({}).then(function() {
     });
+
 })
