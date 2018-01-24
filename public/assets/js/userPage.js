ec16a9e480087458d9a985fd75429d604bf620ac
chaseleinart <chaseleinart@gmail.com>
2018-01-24 11:11:00 -0500

    modified jquery source links to move away from jquery slim: this doesn't support $.ajax methods. also moved userside js src links below jquery so it can work.

--- public/assets/js/userPage.js
+++ public/assets/js/userPage.js
@@ -1,12 +1,36 @@
 $(document).ready(function () {
 
-
+    console.log("this was loaded.")
     // Create a new project function
     $('#submitModalBtn').click(function (e) {
-        console.log($('#projectName').val());
 
+        alert($('#projectName').val());
+        alert($('#inputGroupSelect02').val());
+        alert($('#projectDescription').val());
+
+var Project_name = $('#projectName').val()
+var Project_type = $('#inputGroupSelect02').val()
+var Project_descripton = $('#projectDescription').val()
+var userId = 1
+
+        var sendProject = {
+            Project_name: Project_name,
+            Project_type: Project_type,
+            Project_descripton: Project_descripton,
+            userId: userId
+        };
 
         e.preventDefault();
+console.log(sendProject);
+        // Send the post request.
+        $.ajax("/api/projects/", {
+            type: "POST",
+            data: sendProject
+        }).then(
+            function (result) {
+
+            }
+        );
 
         // Submits a new project and brings user to seacrh page upon completion
         $.post("/api/projects",
