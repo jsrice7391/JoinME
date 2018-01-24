4a92c6b9719ab3c063f2ec8f01f4c459f080424a
Justin Rice <jsrice7391@gmail.com>
2018-01-23 21:46:58 -0500

    Works

--- routes/projects_api_routes.js
+++ routes/projects_api_routes.js
@@ -4,43 +4,20 @@ var path = require("path");
 
 
 module.exports = function(app) {
-    
-//get all projects
 
-      app.get("/api/projects", function(req, res) {
+    //get all projects
+
+    app.get("/api/projects", function(req, res) {
         db.Project.findAll({}).then(function(results) {
             res.json(results);
             // res.render("/project", result);
         })
     });
 
-    // GET route for getting all burgers
-//  router.get("/", function(req, res) {
-//   burger.selectAll(function(data) {
-//       var hbsObject = {
-//           burgers: data
-//       };
-//       res.render("index", hbsObject);
-//   });
-// });
-
-    // GET route for getting all of the posts
-  // app.get("/api/posts", function(req, res) {
-  //   var query = {};
-  //   if (req.query.author_id) {
-  //     query.AuthorId = req.query.author_id;
-  //   }
-  //   db.Post.findAll({
-  //     where: query
-  //   }).then(function(dbPost) {
-  //     res.json(dbPost);
-  //   });
-  // });     
-
     //create a project
     //note that description is misspelled in model
 
-      app.post("/api/projects", function(req, res) {
+    app.post("/api/projects", function(req, res) {
         console.log(req.body)
         db.Project.create({
             Project_name: req.body.Project_name,
@@ -54,18 +31,18 @@ module.exports = function(app) {
 
 
     });
-    
+
     app.delete("/api/projects/:id", function(req, res) {
         // Delete the Project with id from req.body.id
         db.Project.destroy({
-          where: {
-            id: req.body.id
-          }
+            where: {
+                id: req.body.id
+            }
         }).then(function(results) {
-          res.json(results);
-          console.log ("project deleted.")
+            res.json(results);
+            console.log("project deleted.")
         });
-      });
+    });
 
 
-    };
+};
