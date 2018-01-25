afe3be4d5ce0b752be167dade16e5fb96fc76610
emmapankey <emmapankey19@gmail.com>
2018-01-25 09:56:12 -0500

    appending complete a step button to completion column in steps table with jquery

--- public/assets/js/projectPage.js
+++ public/assets/js/projectPage.js
@@ -3,5 +3,7 @@ $(document).ready(function () {
     $(".completeBtn").on("click", function() {
         $(".completeBtn").css("background-color", "#E18942");
     })
+
+    $(".stepCompletionStatus").append( $(".completeBtn" ));
     
 });
